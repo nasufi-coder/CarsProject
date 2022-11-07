3a2b22c1e770289fdf4f411e5082867323e3b8a9
Patrik <patrik.nasufi@gmail.com>
2022-11-07 10:19:03 +0100

    rabbit mq clean code

--- src/main/java/car/com/Cars/service/impl/CarDetailsServiceImpl.java
+++ src/main/java/car/com/Cars/service/impl/CarDetailsServiceImpl.java
@@ -11,7 +11,6 @@ import car.com.Cars.service.CarDetailsService;
 import car.com.Cars.service.rabbit.RabbitMQSender;
 import com.baeldung.openapi.model.CarDTO;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
